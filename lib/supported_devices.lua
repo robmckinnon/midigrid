8d7cf4dd4d989514e30066f0a186d8ff2ff3bf59
robmckinnon <rob@movingflow>
2021-02-06 13:31:43 +0000

    Added comments about LP MIDI device names

--- lib/supported_devices.lua
+++ lib/supported_devices.lua
@@ -2,7 +2,21 @@ local supported_devices = {
   midi_devices = {
     {  midi_base_name= 'launchpad',        device_type='launchpad'     },
     {  midi_base_name= 'launchpad mini',   device_type='launchpad'     },
+    --
+    -- LP Mini MK3 presents two MIDI interfaces over USB:
+    --   launchpad mini mk3 1 - LPMiniMK3 DAW I/O (or first interface): For use
+    --                        by DAWs and similar software to interact with the
+    --                        MK3’s Session mode.
+    --   launchpad mini mk3 2 - LPMiniMK3 MIDI I/O (or second interface): Used
+    --                        to receive MIDI from Custom modes; and to provide
+    --                        external MIDI input or Light controls in
+    --                        Lighting Custom Modes and Programmer mode.
+    --
+    -- LP Mini MK3 needs to be put in Programmer mode manually for midigrid use.
     {  midi_base_name= 'launchpad mini mk3 2', device_type='launchpad_minimk3' },
+    -- A second LP Mini MK3 displays these device names:
+    --   launchpad mini mk3 1 2
+    --   launchpad mini mk3 2 2
     {  midi_base_name= 'launchpad mini mk3 2 2', device_type='launchpad_minimk3_128' },
     {  midi_base_name= 'apc mini',         device_type='apc_mini'      },
     {  midi_base_name= 'block 1',          device_type='livid_block'   },
@@ -22,6 +36,8 @@ function supported_devices.find_midi_device_type(midi_device)
   -- TODO get response to sysex indentify call
 
   if string.lower(midi_device.name):find 'launchpad mini %d' then
+    -- Old launchpad mini's have user set hardware ID 1 - 16:
+    -- e.g. ID 4 appears as midi_device.name "Launchpad Mini 4"
     return 'launchpad'
   else
     for _,device_def in pairs(supported_devices.midi_devices) do
